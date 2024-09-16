05a9dd0455eedbd7b8c172099bd4844badd47369
Bruno Caetano <brunocaetano403@gmail.com>
2024-09-16 18:42:13 -0300

    ex6 e 7 lista vetores

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import aulas.*;
+import libs.Entrada;
 
 public class Main{
 
@@ -8,18 +9,64 @@ public class Main{
 public static void main(String[] args) {
     System.out.println("-- Algoritmos e Estruturas de Dados --");
     // Aula24.main(args);
-    Aula25.Main(args);
+    Aula25.aula();
 
 
-for(int i = 0; i < args.length; i++){
+    int n;
+    int[] v, vPares, vImpares;
+    int i = 0;
+    
 
-    System.out.println(args[i]);
+    Entrada.abrir();
+
+    System.out.println("Quantos números serão digitados? ");
+    n = Entrada.lerInt();
+
+    v = new int [n];
+    vPares = new int [n];
+    vImpares = new int [n];
+   
+    System.out.println("Agora digite os valores: ");
+    for(i = 0; i < n; i++){
+        v[i] = Entrada.lerInt();
+
+    }
+
+    Entrada.fechar();
+
+    int index = 0;
+    int indexImpares = 0;
+
+    for(i = 0; i < n; i++){
+        if (v[i] % 2 == 0) {
+            vPares[index] = v[i];
+            index++;
+        }
+        else{
+            vImpares[indexImpares] = v[i];
+            indexImpares++;
+
+        }
+
+    
+    }
+
+    System.out.println("Pares: " + index);
+        System.out.print("[ ");
+        for(i = 0; i < index; i++){
+            System.out.print(vPares[i] + " ");
+        }
+        System.out.println(" ]");
+
+        System.out.println("Impares: " + indexImpares);
+        System.out.print("[ ");
+        for(i = 0; i < index; i++){
+            System.out.print(vImpares[i] + " ");
+        }
+        System.out.print(" ]");
+
 
 }
 
-
 }
 
-
-
-}
--- aulas/Aula25.java
+++ aulas/Aula25.java
@@ -1,5 +1,7 @@
 package aulas;
 
+import java.util.Arrays;
+
 import libs.Entrada;
 
 public class Aula25 {
@@ -21,7 +23,7 @@ public class Aula25 {
     Entrada.abrir();
     
     System.out.println("valores:");
-    for(i = 0; i <= n; i++){
+    for(i = 0; i < n; i++){
         va[i] = Entrada.lerInt();
 
     }
@@ -51,20 +53,110 @@ public class Aula25 {
 
     }
 
+    public static void paresDepoisImparesPorUltimo(){
 
+        int i = 0;
+        int n = 10;
+        int[] va, vb;
+        int indexAnterior = n - 1;
     
+        va = new int [n];
+        vb = new int [n];
+    
+        Entrada.abrir();
+        
+        System.out.println("valores:");
+        for(i = 0; i <= n; i++){
+            va[i] = Entrada.lerInt();
+    
+        }
+        Entrada.fechar();
+    
+        int index = 0;
+        for(i = 0; i<n; i++){
+            if(va[i] % 2 == 0){
+            vb[index] = va[i];
+            index++;
+    
+            }
+        }
 
-    public static void Main(String[] args){
+       for(i = 0; i < n; i++){
+        if (va[i] % 2 == 0) {
+            vb[indexAnterior] = va[i];
+            indexAnterior--;
+        }
+       }
+    
+        System.out.println("[ ");
+        for(i = 0; i < n; i++){
+            System.out.println(vb[i] + " ");
+        }
+        System.out.println(" ]");
+    
+    
+        }
 
-        aula();
-        paresDepoisImpares();
 
 
 
-    }
-   
+        public static void qtdParesEImpares(){
+
+            //ex7
+
+            int n;
+            int[] v, vPares, vImpares;
+            int i = 0;
+            
+
+            Entrada.abrir();
+
+            System.out.println("Quantos números serão digitados? ");
+            n = Entrada.lerInt();
+        
+            v = new int [n];
+            vPares = new int [n];
+            vImpares = new int [n];
+           
+            System.out.println("Agora digite os valores: ");
+            for(i = 0; i < n; i++){
+                v[i] = Entrada.lerInt();
+        
+            }
+
+            Entrada.fechar();
+
+            int index = 0;
+            int indexImpares = 0;
+
+            for(i = 0; i < n; i++){
+                if (v[i] % 2 == 0) {
+                    vPares[index] = v[i];
+                    index++;
+                }
+                else{
+                    vImpares[indexImpares] = v[i];
+                    indexImpares++;
+
+                }
+
+            
+            }
+
+            System.out.println("Pares: " + index);
+                System.out.println("[ ");
+                for(i = 0; i < index; i++){
+                    System.out.println(vPares[i] + " ]");
+                }
+
+                System.out.println("Impares: " + indexImpares);
+                System.out.println("[ ");
+                for(i = 0; i < index; i++){
+                    System.out.println(vImpares[i] + " ]");
+                }
 
 
+        }
 
 
 }
